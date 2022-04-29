07c42c9ad5eaae9e15b094daf74dfc2376de0bd0
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-04-29 05:19:13 +0100

    running localhost

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app=express()
 const port=process.env.PORT || 8080
  
 app.get("/",(req,res,next)=>{
-    res.send("<h1>Hello docker i am done two</h1>")
+    res.send("<h1>Hello docker I am done and it is working</h1>")
 })
 
 app.listen(port,()=>{
-    console.log("App running  on localhost 8080");
+    console.log("App running on localhost 8080");
 })
